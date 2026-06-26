02ded70336244a9b0a8dfe842dcf1671d331638f
vladislav <vladislav.linnik.1999@gmail.com>
2026-06-26 16:47:51 +0300

    Optimisation for bundle size

--- src/app/features/users/pages/users-page/users-page.ts
+++ src/app/features/users/pages/users-page/users-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { UsersService } from '@features/users/services/users.service';
 import { UsersSearch } from '@features/users/components/users-search/users-search';
 import { UsersTable } from '@features/users/components/users-table/users-table';
+import { NzSkeletonComponent } from 'ng-zorro-antd/skeleton';
 
 @Component({
   selector: 'app-users-page',
-  imports: [UsersSearch, UsersTable],
+  imports: [UsersSearch, UsersTable, NzSkeletonComponent],
   templateUrl: './users-page.html',
   styleUrl: './users-page.css',
   providers: [UsersService],
